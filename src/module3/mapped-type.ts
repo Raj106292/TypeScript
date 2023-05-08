85a73a36324d7800653717d643f47aeb58b5c51b
Raj106292 <rajdas.106292@gmail.com>
2023-05-08 22:24:44 +0600

    module 3-10 done

--- src/module3/mapped-type.ts
+++ src/module3/mapped-type.ts
@@ -1,38 +1,40 @@
-// // run map method on the type
-// const arrayOfNumbers = [ 1, 2, 3];
-// const arrayOfStrings = arrayOfNumbers.map(item => item.toString());
-// console.log(arrayOfStrings);
+// run map method on the type
+const arrayOfNumbers = [ 1, 2, 3];
+const arrayOfStrings = arrayOfNumbers.map(number => number.toString());
+console.log(arrayOfStrings);
 
-// type Volume = {
-//     height: number,
-//     width: number,
-//     depth: number
-// }
+type Volume = {
+    height: number,
+    width: number,
+    depth: number
+}
 
-// type B = Volume["height"]; // look up types
-// type C = keyof Volume; // height | width
+type B = Volume["height"]; // look up types -> set type B as same as height type in Volume
+type C = keyof Volume; // height | width -> union literal type
 
-// type AreaString = {
-//     height: string,
-//     width: string
-// }
+type AreaString = {
+    height: string,
+    width: string
+}
 
-// type AreaReadonly = {
-//     readonly height: number,
-//     readonly width: number
-// }
+type AreaReadonly = {
+    readonly height: number,
+    readonly width: number
+}
 
-// const rectangularArea: AreaReadonly = {
-//     height: 20,
-//     width: 12
-// }
+const rectangularArea: AreaReadonly = {
+    height: 20,
+    width: 12
+}
 
-// type Area<T> = {
-//     // [key in keyof Volume] : Volume[key]; // Volume["height"] -> number
-//     readonly [key in keyof T] : T[key];
-// }
+// rectangularArea.width = 10 // impossible because it is a readonly value
 
-// const area1: Area<{height: number, width: number}> = {
-//     height: 12,
-//     width: 10
-// }
+type Area<T> = {
+    // [key in keyof Volume] : Volume[key]; // Volume["height"] -> number
+    readonly [key in keyof T] : T[key];
+}
+
+const area1: Area<{height: number, width: string}> = {
+    height: 12,
+    width: "10"
+}
