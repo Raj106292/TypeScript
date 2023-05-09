be20426cf4cebfc0759aa6573750d5e81284ec52
Raj106292 <rajdas.106292@gmail.com>
2023-05-09 10:50:40 +0600

    module 4-5 done

---
+++ src/module4/access-modifiers.ts
@@ -0,0 +1,30 @@
+// modify my access -> access modifier
+class BankAccount{
+    id: number;
+    name: string;
+    protected _balance: number;
+
+    constructor(id: number, name: string, balance: number){
+        this.id = id;
+        this.name = name;
+        this._balance = balance;
+    }
+
+    getBalance(){
+        console.log(`My current balance is ${this._balance}`);
+    }
+
+    addDeposit(depositAmount: number){
+        this._balance = this._balance + depositAmount;
+    }
+}
+
+class StudentAccount extends BankAccount{
+    test(){
+        this._balance // private modifier don't allow this
+    }
+}
+
+const myAccount = new BankAccount(106292,"Raj", 500);
+// myAccount._balance = 0;
+console.log("my account", myAccount);
