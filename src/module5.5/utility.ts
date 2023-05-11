cccd52a85d18a21854dc3f25f72603667b7fe41d
Raj106292 <rajdas.106292@gmail.com>
2023-05-12 02:33:04 +0600

    module 5.5-3 modified

--- src/module5.5/utility.ts
+++ src/module5.5/utility.ts
@@ -17,3 +17,27 @@ type Optional = Partial<NewPerson>;
 
 // Required
 type Necessary = Required<NewPerson>
+
+// Readonly
+type MakeReadonly = Readonly<NewPerson>
+
+// type myObject = {
+//     a: string;
+//     b: string;
+//     c: string;
+// }
+
+
+// using index signature
+// type myObject = {
+//     [key: string]: string
+// }
+
+// Record
+type myObject = Record<'a' |'b'|'c', string>
+
+const obj: myObject = {
+    a: "1",
+    b: "2",
+    c: "3"
+}
